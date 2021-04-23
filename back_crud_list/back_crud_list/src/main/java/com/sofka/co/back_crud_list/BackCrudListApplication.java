467f6264a5dc0b5928c0793c2047b64ab35d6fe5
Mauricio Mancera <u20132121837@usco.edu.co>
2021-04-23 13:13:45 -0500

    se crea los repositorios del front y del back

---
+++ back_crud_list/back_crud_list/src/main/java/com/sofka/co/back_crud_list/BackCrudListApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.co.back_crud_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCrudListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCrudListApplication.class, args);
+	}
+
+}
---
+++ back_crud_list/back_crud_list/src/test/java/com/sofka/co/back_crud_list/BackCrudListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.co.back_crud_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCrudListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
