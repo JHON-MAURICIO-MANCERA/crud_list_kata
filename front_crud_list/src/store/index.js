52b77a09607e807475ef4728ec999e491f2043ed
Mauricio Mancera <u20132121837@usco.edu.co>
2021-04-23 13:56:51 -0500

    se confirgura el index donde esta el store provider

---
+++ front_crud_list/src/store/index.js
@@ -0,0 +1,19 @@
+import React, { useReducer, createContext } from 'react';
+import { reducer } from '../reducers/reducer';
+
+    const initialState = {
+        todo: { list: [], item: {},  },
+        groupList: { list: [], item: {},  }
+    };
+  export const Store = createContext(initialState);
+  
+  const StoreProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    
+    return <Store.Provider value={{ state, dispatch }}>
+      {children}
+    </Store.Provider>
+  
+  }
+
+  export default StoreProvider;
